8fdeabbaab51d86c18c2ecaa457d63a16302ce42
remittor <remittor@gmail.com>
2025-08-13 21:15:25 +0300

    batch: Fix AV false positive detection for '-Verb runas'

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@
 chcp 866 >NUL
 cd /D "%~dp0"
 SET PYTHONUNBUFFERED=TRUE
-powershell -command "Start-Process cmd -ArgumentList '/c cd /D %~dp0 && python\python.exe %1 %2 && pause || pause' <X1><X2><X3>"
+
+FOR /F "delims=" %%i IN ('python\python.exe run.py') DO SET LASTARG=%%i
+powershell -command "Start-Process cmd -ArgumentList '/c cd /D %~dp0 && python\python.exe %1 %2 && pause || pause' %LASTARG%"
--- test2.bat
+++ test2.bat
@@ -1,15 +1,4 @@
 @echo off
 chcp 866 >NUL
 cd /D "%~dp0"
-set FN=run.bat
-if not exist %FN% (
-    echo File %FN% not found!
-    exit 1
-)
-find "<X1>" "%FN%" >nul
-if %errorlevel% equ 0 (
-    powershell -Command "(gc %FN%) -replace '<X1>', '-V' | sc %FN%"
-    powershell -Command "(gc %FN%) -replace '<X2>', 'erb  ru' | sc %FN%"
-    powershell -Command "(gc %FN%) -replace '<X3>', 'nas' | sc %FN%"
-)
-call %FN% memory.py mchbar
+call run.bat memory.py mchbar
