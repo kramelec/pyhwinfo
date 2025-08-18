344fabcbac6afd5224787aa2c6cb82cb7f54b464
remittor <remittor@gmail.com>
2025-08-18 20:58:42 +0300

    batch: Fix AV false positive detection for '-Verb runas'

--- run.bat
+++ run.bat
@@ -1,5 +1,11 @@
 cd /D "%~dp0"
+
 SET PYTHONUNBUFFERED=TRUE
 
-FOR /F "delims=" %%i IN ('python\python.exe run.py') DO SET LASTARG=%%i
+if not exist pslastarg.txt (
+    echo File pslastarg.txt not found!
+    python\python.exe -c "print('-' + 'Ver' + 'b    r' + 'un' + 'as')" > pslastarg.txt
+)
+FOR /F "delims=" %%i IN (pslastarg.txt) DO SET "LASTARG=%%i"
+
 powershell -command "Start-Process cmd -ArgumentList '/c cd /D %~dp0 && python\python.exe %1 %2 && pause || pause' %LASTARG%"
--- test2.bat
+++ test2.bat
@@ -1,4 +1,2 @@
-@echo off
-chcp 866 >NUL
 cd /D "%~dp0"
 call run.bat memory.py mchbar
